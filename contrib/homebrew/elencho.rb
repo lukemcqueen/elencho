4693d3219aa4ba0a01ce291041559c8f68466346
Luke McQueen <lukemcqueen@gmail.com>
2026-06-24 16:48:31 +0900

    Bump version to 0.3.0

--- contrib/homebrew/elencho.rb
+++ contrib/homebrew/elencho.rb
@@ -5,7 +5,7 @@ class Elencho < Formula
   desc "Supply-chain malware and obfuscation scanner"
   homepage "https://github.com/lukemcqueen/elencho"
   license "MIT"
-  version "0.2.0"
+  version "0.3.0"
 
   on_macos do
     if Hardware::CPU.arm?
