67ab2781eeeead6d771aae831b80de20955b55f9
Luke McQueen <lukemcqueen@gmail.com>
2026-06-24 17:02:42 +0900

    Bump version to 0.3.1

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,7 @@ package config
 
 // Version is the current version of Elencho.
 // Overridden at build time via -ldflags.
-var Version = "0.3.0"
+var Version = "0.3.1"
 
 // Default constants
 const (
