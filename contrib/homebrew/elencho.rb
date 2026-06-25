8a014c7be1016bf0d18e70f252bed48b244dbc81
Luke McQueen <lukemcqueen@gmail.com>
2026-06-25 13:29:34 +0900

    Update Homebrew formula with v0.4.0 SHAs

--- contrib/homebrew/elencho.rb
+++ contrib/homebrew/elencho.rb
@@ -9,21 +9,21 @@ class Elencho < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/lukemcqueen/elencho/releases/download/v0.2.0/elencho-darwin-arm64"
-      sha256 "5050387a4c7058968df17d8a2e403ed33225c599fb53358a88e24c8ea030089d"
+      url "https://github.com/lukemcqueen/elencho/releases/download/v0.4.0/elencho-darwin-arm64"
+      sha256 "2e1bbff917e6cec86558e7256651fb797ca3144d80d8ee37133e114c66d5bb87"
     else
-      url "https://github.com/lukemcqueen/elencho/releases/download/v0.2.0/elencho-darwin-amd64"
-      sha256 "2950eefab0f1af7f9afe3544b7350454ea8b6883729b5c60695bec202084f933"
+      url "https://github.com/lukemcqueen/elencho/releases/download/v0.4.0/elencho-darwin-amd64"
+      sha256 "ede5468fe8500a9a784b899700d0dae66a316a9922ccf7c749b11bb54f2d1762"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/lukemcqueen/elencho/releases/download/v0.2.0/elencho-linux-arm64"
-      sha256 "12742f776c4707d9568be32df8c49a63bf61fc2f35a48d75bfa71d0f2e5d861e"
+      url "https://github.com/lukemcqueen/elencho/releases/download/v0.4.0/elencho-linux-arm64"
+      sha256 "7aef6740a0351d1b54ae35527c16ed12bb7ce37161afc5d2804d41b9099e48bf"
     else
-      url "https://github.com/lukemcqueen/elencho/releases/download/v0.2.0/elencho-linux-amd64"
-      sha256 "ff32c07c01e4a549c5cabeb06fa8c407dd1ed3d4ca50b850f59b169c92254d2a"
+      url "https://github.com/lukemcqueen/elencho/releases/download/v0.4.0/elencho-linux-amd64"
+      sha256 "40efcd168ca129b5876f18aa920baf652070bd98872d2e8b190807386b2ec4c2"
     end
   end
 
